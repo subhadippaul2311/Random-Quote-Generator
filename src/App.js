19708bca5915bea9d12107ecf3d9665b8f88e541
subhadippaul2311 <126654387+subhadippaul2311@users.noreply.github.com>
2023-05-18 00:03:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuoteGenerator from './Quote';
+
+const App = () => {
+  return (
+    <div className="App">
+      <QuoteGenerator/>
+    </div>
+  );
+};
+
+export default App;

e577c635a745a9c2935036030f36e8b8717a5246
subhadippaul2311 <126654387+subhadippaul2311@users.noreply.github.com>
2023-05-18 00:04:25 +0530

    2 files modified

--- src/Quote.js
+++ src/Quote.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import "./Quote.css"
+import './Quote.css';
 const quotes = [
-  "The greatest glory in living lies not in never falling, but in rising every time we fall",
+  'The greatest glory in living lies not in never falling, but in rising every time we fall',
   "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough",
-  "The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart",
-  "You will face many defeats in life, but never let yourself be defeated",
+  'The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart',
+  'You will face many defeats in life, but never let yourself be defeated',
   "Keep smiling, because life is a beautiful thing and there's so much to smile about",
-  "Success is not final; failure is not fatal: It is the courage to continue that counts",
+  'Success is not final; failure is not fatal: It is the courage to continue that counts',
   "Nothing is impossible, the word itself says, ‘I'm possible!",
-  "Keep your face always toward the sunshine, and shadows will fall behind you.",
-  "The love of money is the root of all evil",
-  "Opportunities don't happen, you create them"
+  'Keep your face always toward the sunshine, and shadows will fall behind you.',
+  'The love of money is the root of all evil',
+  "Opportunities don't happen, you create them",
 ];
 
 const QuoteGenerator = () => {
